dccd8f7d040daff66c340a40e11c170859218f26
MariusMM2 <dmunteanu91@gmail.com>
2020-11-25 22:26:35 +0100

    added comments; cleaned up code a bit

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,6 @@
 /**
  * This file contains database-related functions.
  */
-
 const config = require("./config");
 const {MongoClient, ObjectId} = require('mongodb');
 
@@ -12,28 +11,46 @@ module.exports.MongoDatabase = class MongoDatabase {
         this._collectionName = collectionName;
     }
 
+    /**
+     * Inserts an item in the database.
+     * @param item - Item to be inserted in database
+     * @returns {Promise<ObjectId|undefined>} - Id of the new item or 'undefined' in case of errors
+     */
     async create(item) {
+        // create a MongoClient instance and connect to the database
         const client = await MongoClient.connect(config.connectionUrl);
+        // get a reference to the collection of this MongoDatabase instance
         const collection = client.db(config.databaseName).collection(this._collectionName);
 
         try {
+            // insert the item in the database
             const writeResult = await collection.insertOne(item);
+            // return the ObjectId of the new item
             return writeResult.insertedId;
         } catch (err) {
             console.log(err);
+            // return 'undefined' if any error occurred
             return undefined;
         } finally {
+            // close the client connection to the database
             await client.close();
         }
     }
 
+    /**
+     * Retrieves a single item from the database.
+     * @param id - Id of the item to retrieve
+     * @returns {Promise<*|undefined>} - Matching item; null if not found or 'undefined' in case of errors
+     */
     async getOne(id) {
         const client = await MongoClient.connect(config.connectionUrl);
         const collection = client.db(config.databaseName).collection(this._collectionName);
 
         try {
+            // if a string id is given, convert it to ObjectId
             if (typeof id === "string") id = new ObjectId(id);
 
+            // query object
             const query = {_id: id};
 
             return await collection.findOne(query);
@@ -45,23 +62,37 @@ module.exports.MongoDatabase = class MongoDatabase {
         }
     }
 
+    /**
+     * Retrieves all items from the database.
+     * @returns {Promise<*|undefined>} - Matching item list or 'undefined' in case of errors
+     */
     async getAll() {
         const client = await MongoClient.connect(config.connectionUrl);
         const collection = client.db(config.databaseName).collection(this._collectionName);
 
+        // ready mongodb cursor
         const cursor = collection.find();
 
         try {
+            // convert cursor documents to array
             return await cursor.toArray();
         } catch (err) {
             console.log(err);
             return undefined;
         } finally {
             await client.close();
+            // close cursor
             await cursor.close();
         }
     }
 
+    /**
+     * Updates a single item in the database.
+     * @param id - Id of the item to update
+     * @param item - Object with the new attributes of the item
+     * @returns {Promise<*|null|undefined>} - Updated item; 'null' if no changes took place
+     * or 'undefined' in case of other errors
+     */
     async updateOne(id, item) {
         const client = await MongoClient.connect(config.connectionUrl);
         const collection = client.db(config.databaseName).collection(this._collectionName);
@@ -71,15 +102,19 @@ module.exports.MongoDatabase = class MongoDatabase {
 
             const query = {_id: id};
 
+            // document that specifies update actions for the existing object
             const updateDocument = {
                 $set: item
             };
 
             const result = await collection.updateOne(query, updateDocument);
+            // check if any documents have been modified
             if (result.result.nModified === 1) {
+                // one document has been updated, return it
                 return this.getOne(id);
             } else {
-                return undefined;
+                // no documents updated, return 'null'
+                return null;
             }
         } catch (err) {
             console.log(err);
@@ -89,6 +124,12 @@ module.exports.MongoDatabase = class MongoDatabase {
         }
     }
 
+    /**
+     * Deletes a single item in the database.
+     * @param id - Id of the item to delete
+     * @returns {Promise<*|null|undefined>} - Deleted item; 'null' if no deletes took place
+     * or 'undefined' in case of other errors
+     */
     async deleteOne(id) {
         const client = await MongoClient.connect(config.connectionUrl);
         const collection = client.db(config.databaseName).collection(this._collectionName);
@@ -98,14 +139,17 @@ module.exports.MongoDatabase = class MongoDatabase {
 
             const query = {_id: id};
 
+            // retrieve the old item from the database
             const oldItem = await this.getOne(id);
 
             const result = await collection.deleteOne(query);
-            console.log(result);
+            // check if any documents have been deleted
             if (result.deletedCount === 1) {
+                // one document has been deleted, return it
                 return oldItem;
             } else {
-                return undefined;
+                // no documents deleted, return 'null'
+                return null;
             }
         } catch (err) {
             console.log(err);
--- server/routes/review.js
+++ server/routes/review.js
@@ -6,53 +6,83 @@ const express = require("express");
 const {inputValidator, createHandler, readHandler, updateHandler, deleteHandler} = require("./restMiddleware");
 const {MongoDatabase} = require("../db/database");
 const {reviewCol} = require("../db/config");
-const {body, param} = require("express-validator");
+const {body, param, ValidationChain} = require("express-validator");
 const router = express.Router();
 
 const db = new MongoDatabase(reviewCol);
 
+/**
+ * Function that returns a {@link ValidationChain} used to
+ * validate the structure of the 'id' parameter in the request URL.
+ * @returns {function} - ValidationChain object
+ */
 function getParamIdValidation() {
     return param('id')
+        // ensure 'id' is provided
         .exists().withMessage("must be provided").bail()
+        // check if is a valid ObjectId string
         .isMongoId().withMessage("must be a valid MongoDB ObjectId string");
 }
 
 // create
 router.post('/',
+    // 'userId' body attribute
     body('userId')
+        // ensure 'userId' is provided
         .exists().withMessage("must be provided").bail()
+        // check if is a valid ObjectId string
         .isMongoId().withMessage("must be a valid MongoDB ObjectId string"),
+    // 'content' body attribute
     body('content')
+        // ensure 'content' is provided
         .exists().withMessage("must be provided").bail()
+        // ensure length is between 10 and 1000 characters
         .isLength({min: 10, max: 1000}).withMessage('must be between 10 and 1000 characters'),
+    // validate above attributes
     inputValidator,
+    // handle create
     createHandler(db, "userId", "content"));
 
 // read
 router.get('/:id',
+    // 'id' URL param
     getParamIdValidation(),
+    // validate above attributes
     inputValidator,
+    // handle read
     readHandler(db, "id"));
 
 // read all
-router.get('/', readHandler(db));
+router.get('/',
+    // handle read all
+    readHandler(db));
 
 // update
 router.patch('/:id',
+    // 'id' URL param
     getParamIdValidation(),
+    // 'userId' body attribute
     body('userId')
+        // only run validation if 'userId' is provided
         .if(body('userId').exists())
+        // check if is a valid ObjectId string
         .isMongoId().withMessage("must be a valid MongoDB ObjectId string"),
+    // 'content' body attribute
     body('content')
+        // only run validation if 'content' is provided
         .if(body('content').exists())
+        // ensure length is between 10 and 1000 characters
         .isLength({min: 10, max: 1000}).withMessage('must be between 10 and 1000 characters'),
     // validate above attributes
     inputValidator,
+    // handle update
     updateHandler(db, "userId", "content"));
 
 // delete
 router.delete('/:id',
+    // 'id' URL param
     getParamIdValidation(),
+    // handle delete
     deleteHandler(db));
 
 module.exports = router;
