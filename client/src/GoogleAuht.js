c96e146afc81891432b8b73194f28bb0bd1ca3d9
Hakan Aslan <33924602+Hakan2703@users.noreply.github.com>
2020-11-28 18:27:40 +0100

    google js file

--- client/src/GoogleAuht.js
+++ client/src/GoogleAuht.js
@@ -3,6 +3,43 @@ const express = require('express');
 
 const app = express();
 
+import * as firebase from 'firebase'
+import './logreg.css'
+import firebaseConfig from "client\public\firebase.confiq.js";
+
+// Initialize Firebase
+//firebase.initializeApp(firebaseConfig);
+//var provider = new firebase.auth.GoogleAuthProvider();
+//provider.add2FactorAuthentication(userID)
+//{
+    // get the secret to be shared with the google authenticator app
+//    const gaSecret = await generateSecret(); 
+    //await DB.TheTable.update(userDI, {gaSecret});
+//}
+
+const {verifyToken} = require('@authentication/google-authenticator');
+
+function onToken(token) {
+  if (verifyToken({secret: user.gaSecret, token}) === true) {
+    // verified token
+  }
+}
+
+const handleForm = e => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        if (res.user) Auth.setLoggedIn(true);
+      })
+      .catch(e => {
+        setErrors(e.message);
+      });
+  };
+
+
+
 const googleAuthentication = new GoogleAuthentication({
   callbackURL: '/__/auth/google'
 });
@@ -18,7 +55,7 @@ app.get('/', (req, res, next) => {
 app.post(googleAuthentication.callbackPath, async (req, res, next) => {
   googleAuthentication.redirectToProvider(req, res, next, {
     // you can pass some abritrary state through to the callback here
-    state: {message: 'Hello world'}
+    state: {message: ' '}
   });
 });
 app.get(googleAuthentication.callbackPath, async (req, res, next) => {
@@ -30,7 +67,7 @@ app.get(googleAuthentication.callbackPath, async (req, res, next) => {
       accessToken, // use this to make requests to the Google API on behalf of the user
       refreshToken,
       profile,
-      state // => {message: 'Hello world'}
+      state 
     } = await googleAuthentication.completeAuthentication(req, res);
     res.json(profile);
   } catch (ex) {
@@ -39,3 +76,7 @@ app.get(googleAuthentication.callbackPath, async (req, res, next) => {
 });
 
 app.listen(3001);
+
+export const signInWithGoogle = () => {
+    auth.signInWithPopup(provider);
+  };
