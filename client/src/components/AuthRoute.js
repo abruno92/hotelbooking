095556ac018289808c0bd02940091989180f3107
MariusMM2 <dmunteanu91@gmail.com>
2020-12-14 21:19:58 +0100

    fixed typo

--- client/src/components/AuthRoute.js
+++ client/src/components/AuthRoute.js
@@ -5,7 +5,7 @@ import {Redirect, Route} from "react-router";
  * A Route guarded by the current state of the
  * authenticated user.
  * @param {React.Component} Component Component target of the route
- * @param {boolean} mustAuth Whether or not the user be authenticated to access this route
+ * @param {boolean} mustAuth Whether or not the user must be authenticated to access this route
  * @param {boolean} canAccess Whether or not the user can access the route at this time
  * @param rest Remaining Route attributes to be passed to the Route component
  * @returns {JSX.Element}
